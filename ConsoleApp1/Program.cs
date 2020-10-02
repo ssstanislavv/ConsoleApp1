f455a246fc0bd9bda319fab720c3a6b8a0aed942
Admin <matv860@gmail.com>
2020-10-02 10:47:59 +0300

    Изменил некоторые строки программы

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //Этот метод должен возвращать случайное значение
+        static int GetRandom()
+        {
+            //Создание объекта для генерации чисел (с указанием начального значения)
+            Random rnd = new Random(245);
+
+            //Получить случайное число 
+            int value = rnd.Next();
+
+            //Вернуть полученное значение
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            //Вывод сгенерированных чисел в консоль
+            Console.WriteLine(GetRandom());
+            Console.WriteLine(GetRandom());
+            Console.WriteLine(GetRandom());
+        }
+    }
+}
