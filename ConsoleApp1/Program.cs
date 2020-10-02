a970371615034f7583bf0758a3e233b1d52aa7c9
Admin <matv860@gmail.com>
2020-10-02 10:49:53 +0300

    Создал еще один коммит

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,22 +4,22 @@ namespace ConsoleApp1
 {
     class Program
     {
-        //Этот метод должен возвращать случайное значение
+      
         static int GetRandom()
         {
-            //Создание объекта для генерации чисел (с указанием начального значения)
-            Random rnd = new Random(245);
+         
+            Random rnd = new Random(246);
 
             //Получить случайное число 
             int value = rnd.Next();
 
-            //Вернуть полученное значение
+          
             return value;
         }
 
         static void Main(string[] args)
         {
-            //Вывод сгенерированных чисел в консоль
+    
             Console.WriteLine(GetRandom());
             Console.WriteLine(GetRandom());
             Console.WriteLine(GetRandom());
